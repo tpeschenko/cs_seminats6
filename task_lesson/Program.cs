4e024e377cb9201d4e3399c4db0434a16ee084ca
Taras <taras.peschenko.90@mail.ru>
2022-12-07 17:58:18 +0300

    Перевернуть массив(2 способа)

--- task_lesson/Program.cs
+++ task_lesson/Program.cs
@@ -6,31 +6,39 @@ void task39()
 {
     int[] array = {1, 3, 6, 3, 3};
     int sizeArray = array.Length;
-    int[] newArray = new int[sizeArray];
-    string str = String.Empty;
+    int size = sizeArray - 1;
+    int temp = 0;
 
-    TurnArray(array);
+    
+    // TurnArray_1(array);
+    TurnArray_2(array);
     PrintArray(array);
     
+//  Первый способ(через переменную)
+//    void TurnArray_1(int[] num)
+//    {    
+//         for(int i = 0; i < sizeArray/2; i++)
+//         {   
+//             temp = num[i];
+//             num[i] = num[size - i];
+//             num[size - i] = temp;
 
-   void TurnArray(int[] num)
-   {
-        for(int i = sizeArray - 1; i >= 0; i--)
+//         }
+
+//  Второй способ, через кортеж. 
+    void TurnArray_2(int[] num)
+   {    
+        for(int i = 0; i < sizeArray/2; i++)
         {   
-            str += num[i];                
-        }
-        for(int i = 0; i < sizeArray; i++)
-        {
-            array[i] = Convert.ToInt32(str[i]);
+            (num[i], num[size - i]) = (num[size - i], num[i]);
         }
    }
    void PrintArray(int[] num)
     {
-        int LonArray = num.Length;
         Console.WriteLine("Перевёрнутый массив: ");
         for(int i = 0; i < sizeArray; i++)
         {
-            Console.Write(str[i] + " ");
+            Console.Write(num[i] + " ");
         }
         Console.WriteLine();
     }
