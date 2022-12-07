57639eeaa11e32390922c542fbff7af22c5d1952
Taras <taras.peschenko.90@mail.ru>
2022-12-07 19:48:31 +0300

    Свойства треугольника

---
+++ task_lesson_2/Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу, которая принимает на вход три
+// числа и проверяет, может ли существовать треугольник со
+// сторонами такой длины.
+
+void Task39()
+{
+    FindMaxSide(2, 8, 5);
+
+}    
+
+void FindMaxSide(int sideA, int sideB, int sideC)
+{
+    if((sideA < sideB + sideC) && (sideB < sideA + sideC) && (sideC < sideA + sideB))
+    {
+        Console.Write("Треугольник возможен ");
+    }
+    else
+    {
+        Console.Write("Треугольник не возможен");
+    }
+ 
+}
+
+Task39();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
